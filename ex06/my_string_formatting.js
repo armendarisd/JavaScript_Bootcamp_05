693f7209e4eb6375d558abc973dbe4b2eef6ff72
daniel barrera <barrera_d@qwasar.io>
2021-10-02 02:20:07 +0000

    Last version

---
+++ ex06/my_string_formatting.js
@@ -0,0 +1,6 @@
+function my_string_formatting(firstname, lastname, age){
+    let value;
+    value = "Hello, my name is "+firstname+" "+lastname+", I'm "+age.toString()+".";
+    console.log(value);
+    return value;
+};
